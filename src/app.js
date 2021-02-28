0e5fead3a932a7ded4b330354f96b4eced6e61e2
Krzysztof Grudzień <krzysztof.grudzien.dev@gmail.com>
2021-02-28 23:27:19 +0100

    recreate DOM elements as variables for JS

--- src/app.js
+++ src/app.js
@@ -3,69 +3,74 @@ import DOMElements from './helpers/DOMElements';
 import IDGenerator from './helpers/IDGenerator';
 import FormatCost from './helpers/FormatCost';
 
-const allBills = [];
-let walletAmount = 2000;
-let walletSpending = 0;
-
-const addBill = () => {
-    let id = IDGenerator();
-    let billValue = FormatCost(DOMElements.billInput.value);
-
-    if (billValue > 0) {
-        allBills.push({
-            billValue,
-            id,
-        });
-
-        DOMElements.billInput.value = '';
-
-        const labelContainer = document.createElement('div');
-        labelContainer.classList = 'label';
-        const labelContent = `
-            <span class="label__price">$${billValue}</span> 
-            <img
-                src="./src/assets/icons/clear.svg" 
-                class="label__delete" 
-            /> 
-        `;
-        labelContainer.setAttribute('data-id', `${id}`);
-        labelContainer.innerHTML = labelContent;
-        DOMElements.cardLabels.appendChild(labelContainer);
-
-        walletSpending += billValue;
-        walletAmount -= billValue;
-
-        DOMElements.walletSpending.textContent = `$${FormatCost(
-            walletSpending,
-        )}`;
-        DOMElements.totalCost.textContent = `$${FormatCost(walletSpending)}`;
-        DOMElements.walletAmount.textContent = `$${FormatCost(walletAmount)}`;
-    } else {
-        return;
-    }
-
-    DOMElements.totalSplitCost.textContent = `$${FormatCost(walletSpending)}`;
-    DOMElements.totalTip.textContent = `$${FormatCost(
-        (walletSpending / 100) * 2,
-    )}`;
+const calculateBill = () => {
+    console.log('ok');
 };
 
-DOMElements.btnAddBill.addEventListener('click', addBill);
+DOMElements.btnCalculate.addEventListener('click', calculateBill);
 
-const percentTip = e => {
-    DOMElements.percentageTip.textContent = `${e.target.value}%`;
-    DOMElements.totalTip.textContent = `$${FormatCost(
-        (e.target.value * walletSpending) / 100,
-    )}`;
-};
-
-DOMElements.percentageRange.addEventListener('input', percentTip);
-
-const splitTip = e => {
-    DOMElements.splitTip.textContent = e.target.value;
-    DOMElements.totalSplitCost.textContent = `$${FormatCost(
-        walletSpending / e.target.value,
-    )}`;
-};
-
-DOMElements.splitRange.addEventListener('input', splitTip);
+// const allBills = [];
+//
+// const calculateBill = () => {
+// let id = IDGenerator();
+// let billValue = FormatCost(DOMElements.billInput.value);
+//
+// if (billValue > 0) {
+// allBills.push({
+// billValue,
+// id,
+// });
+//
+// DOMElements.billInput.value = '';
+//
+// const labelContainer = document.createElement('div');
+// labelContainer.classList = 'label';
+// const labelContent = `
+// <span class="label__price">$${billValue}</span>
+// <img
+// src="./src/assets/icons/clear.svg"
+// class="label__delete"
+// />
+// `;
+// labelContainer.setAttribute('data-id', `${id}`);
+// labelContainer.innerHTML = labelContent;
+// DOMElements.cardLabels.appendChild(labelContainer);
+//
+// walletSpending += billValue;
+// walletAmount -= billValue;
+//
+// DOMElements.walletSpending.textContent = `$${FormatCost(
+// walletSpending,
+// )}`;
+// DOMElements.totalCost.textContent = `$${FormatCost(walletSpending)}`;
+// DOMElements.walletAmount.textContent = `$${FormatCost(walletAmount)}`;
+// } else {
+// return;
+// }
+//
+// DOMElements.totalSplitCost.textContent = `$${FormatCost(walletSpending)}`;
+// DOMElements.totalTip.textContent = `$${FormatCost(
+// (walletSpending / 100) * 2,
+// )}`;
+// };
+//
+// DOMElements.btnAddBill.addEventListener('click', calculateBill);
+//
+// const percentTip = e => {
+// DOMElements.percentageTip.textContent = `${e.target.value}%`;
+// DOMElements.totalTip.textContent = `$${FormatCost(
+// (e.target.value * walletSpending) / 100,
+// )}`;
+// };
+//
+// DOMElements.percentageRange.addEventListener('input', percentTip);
+//
+// const splitTip = e => {
+// DOMElements.splitTip.textContent = e.target.value;
+// DOMElements.totalSplitCost.textContent = `$${FormatCost(
+// walletSpending / e.target.value,
+// )}`;
+// };
+//
+// DOMElements.splitRange.addEventListener('input', splitTip);
+//
--- src/helpers/DOMElements.js
+++ src/helpers/DOMElements.js
@@ -1,29 +1,71 @@
-const totalTip = document.querySelector('.card__total-tip--js');
-const totalCost = document.querySelector('.card__total-cost--js');
+// const totalTip = document.querySelector('.card__total-tip--js');
+// const totalCost = document.querySelector('.card__total-cost--js');
+// const percentageTip = document.querySelector('.card__percentage-tip--js');
+// const percentageRange = document.querySelector('.card__percentage-range--js');
+// const splitTip = document.querySelector('.card__split-tip--js');
+// const splitRange = document.querySelector('.card__split-range--js');
+// const totalSplitCost = document.querySelector('.card__total-split-cost--js');
+// const billInput = document.querySelector('.add-bill__input--js');
+// const btnAddBill = document.querySelector('.add-bill__btn--js');
+// const cardLabels = document.querySelector('.card__labels--js');
+//
+// const DOMElements = {
+// totalTip,
+// totalCost,
+// percentageTip,
+// percentageRange,
+// splitTip,
+// splitRange,
+// totalSplitCost,
+// billInput,
+// btnAddBill,
+// cardLabels,
+// };
+
+const inputNumber = document.querySelector('.card__input--number--js');
+const inputText = document.querySelector('.card__input--text--js');
+const selectCurrency = document.querySelector('.card__select--js');
 const percentageTip = document.querySelector('.card__percentage-tip--js');
 const percentageRange = document.querySelector('.card__percentage-range--js');
+const percentageTipEnd = document.querySelector(
+    '.card__percentage-tip-end--js',
+);
 const splitTip = document.querySelector('.card__split-tip--js');
 const splitRange = document.querySelector('.card__split-range--js');
-const totalSplitCost = document.querySelector('.card__total-split-cost--js');
-const walletAmount = document.querySelector('.card__wallet-amount--js');
-const walletSpending = document.querySelector('.card__wallet-spending--js');
-const billInput = document.querySelector('.add-bill__input--js');
-const btnAddBill = document.querySelector('.add-bill__btn--js');
-const cardLabels = document.querySelector('.card__labels--js');
+const splitTipEnd = document.querySelector('.card__split-tip-end--js');
+const btnCalculate = document.querySelector('.btn--js');
+const btnOpenApp = document.querySelector('.btn--open--js');
+const itemBill = document.querySelector('.card__item-bill--js');
+const itemNameBill = document.querySelector('.card__item-name-bill--js');
+const itemPercentTip = document.querySelector('.card__item-percent-tip--js');
+const itemTotalTip = document.querySelector('.card__item-total-tip--js');
+const itemSplit = document.querySelector('.card__item-split--js');
+const itemBillPerson = document.querySelector('.card__item-bill-person--js');
+const itemBillTip = document.querySelector('.card__item-bill-tip--js');
+const itemBillCost = document.querySelector('.card__item-bill-cost--js');
+const calculator = document.querySelector('.calculator--js');
 
 const DOMElements = {
-    totalTip,
-    totalCost,
+    inputNumber,
+    inputText,
+    selectCurrency,
     percentageTip,
     percentageRange,
+    percentageTipEnd,
     splitTip,
+    splitTipEnd,
+    btnCalculate,
+    btnOpenApp,
     splitRange,
-    totalSplitCost,
-    walletAmount,
-    walletSpending,
-    billInput,
-    btnAddBill,
-    cardLabels,
+    itemBill,
+    itemNameBill,
+    itemPercentTip,
+    itemTotalTip,
+    itemSplit,
+    itemBillPerson,
+    itemBillTip,
+    itemBillCost,
+    calculator,
 };
 
 export default DOMElements;
